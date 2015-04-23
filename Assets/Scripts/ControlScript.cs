5546e587547c7200d02e86439e069234b62efcbd
Adamsc <sco3.1415@gmail.com>
2015-04-23 11:04:04 +0200

    Initialize
    Metaball "pre-working"
    Basic physics controller for ball

---
+++ Assets/Scripts/ControlScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlScript : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("up")) {
+			transform.Translate(Vector3.forward * speed);
+		}
+		if (Input.GetKey("down")) {
+			transform.Translate(-Vector3.forward * speed);
+		}
+		if (Input.GetKey("left")) {
+			transform.Translate(Vector3.left * speed);
+		}
+		if (Input.GetKey("right")) {
+			transform.Translate(Vector3.right * speed);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/PhysicsControlScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsControlScript : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+
+	private Rigidbody2D rBody;
+	void Start () {
+
+		rBody = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("up")) {
+			rBody.AddForce(Vector3.up * speed);
+		}
+		if (Input.GetKey("down")) {
+			rBody.AddForce(-Vector3.up * speed);
+		}
+		if (Input.GetKey("left")) {
+			rBody.AddForce(Vector3.left * speed);
+		}
+		if (Input.GetKey("right")) {
+			rBody.AddForce(Vector3.right * speed);
+		}
+		
+	}
+}
